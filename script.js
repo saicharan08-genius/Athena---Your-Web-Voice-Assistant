ebec81b52492fe76b8e570690086760b1c8b3a5d
Saicharan Mugunthan <70827777+saicharan08-genius@users.noreply.github.com>
2021-03-01 16:29:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+alert("Hey! It is Athena! Be sure your commands are loud and clear")
+
+responsiveVoice.speak("hi, I am Athena, How may I help you" , "US English Female");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+if(annyang){
+
+  annyang.start();
+
+  const googlesearch = {
+    '(Hey) (Athena) (Hey) (man) search (for) *searchQuery': moveWeb
+  };
+
+  const ytsearch = {
+    '(Hey) (Athena) (Hey) (man) (go to) youtube (and) search *searchQuery1' : ytsearch1
+  };
+
+  const athenawiki = {
+    '(Athena) what (is) your name (Athena)' : answer,
+    '(Athena) who (made) (built) (build) you (Athena)' : reply1
+    '(Athena) who are you (Athena)' : reply2,
+    '(Athena) where do you (reside) (live) (stay) (Athena)' : reply3,
+    '(Athena) how are you (Athena)' : reply4,
+    '(Athena) how its going (Athena)' : reply4,
+    '(Athena) Are you (a)(an) (robot)(aeai)(artificial intelligence)' : reply5,
+    '(Athena) sup (Athena)' : reply6,
+    '(Athena) sups (Athena)' : reply6,    
+  };
+
+  const wikisearch = {
+    //code
+  };
+
+  annyang.addCommands(googlesearch);
+  annyang.addCommands(ytsearch);
+  annyang.addCommands(athenawiki);
+  annyang.addCommands(wikisearch);
+
+  function moveWeb(searchQuery) {
+    window.location.replace('https://google.com/search?q=' + searchQuery);
+  }
+
+  function ytsearch1(searchQuery1) {
+    window.location.replace('https://www.youtube.com/results?search_query=' + searchQuery1);
+  }
+
+  function answer() {
+    const names = ['My name is Athena' , 'Hey! My name is Athena!' , 'Hola! My name is Athena'];
+    var i = parseInt(getRandomInt(3));
+    responsiveVoice.speak(names[i] , "US English Female");
+  }
+  
+  function reply1(){
+    //code
+  }
+
+  function reply2(){
+    //code
+  }
+
+}
